9a5066a873e732d288f96f9f3ac6cb9f4632fd61
herizon1054 <spivbaby@gmail.com>
2026-01-29 00:52:50 +0800

    Update radar_interface.py

--- opendbc_repo/opendbc/car/toyota/radar_interface.py
+++ opendbc_repo/opendbc/car/toyota/radar_interface.py
@@ -5,8 +5,8 @@ from opendbc.car.structs import RadarData
 from opendbc.car.toyota.values import DBC, TSS2_CAR
 from opendbc.car.interfaces import RadarInterfaceBase
 
-
 def _create_radar_can_parser(car_fingerprint):
+  # 根據車型選擇雷達訊息 ID 範圍
   if car_fingerprint in TSS2_CAR:
     RADAR_A_MSGS = list(range(0x180, 0x190))
     RADAR_B_MSGS = list(range(0x190, 0x1a0))
@@ -16,12 +16,13 @@ def _create_radar_can_parser(car_fingerprint):
 
   msg_a_n = len(RADAR_A_MSGS)
   msg_b_n = len(RADAR_B_MSGS)
+  # 建立訊息列表，頻率設定為 20Hz
   messages = list(zip(RADAR_A_MSGS + RADAR_B_MSGS, [20] * (msg_a_n + msg_b_n), strict=True))
 
   return CANParser(DBC[car_fingerprint][Bus.radar], messages, 1)
 
-
 class RadarInterface(RadarInterfaceBase):
+  # 【修正】: 移除 CP_SP 參數，只保留 CP，避免 TypeError
   def __init__(self, CP):
     super().__init__(CP)
     self.track_id = 0
@@ -63,24 +64,29 @@ class RadarInterface(RadarInterfaceBase):
       if ii in self.RADAR_A_MSGS:
         cpt = self.rcp.vl[ii]
 
+        # 【保留原邏輯】: 超過 255m 視為無效並重置計數器
         if cpt['LONG_DIST'] >= 255 or cpt['NEW_TRACK']:
           self.valid_cnt[ii] = 0    # reset counter
+        
+        # 【保留原邏輯】: 只有距離小於 255m 才增加有效計數
         if cpt['VALID'] and cpt['LONG_DIST'] < 255:
           self.valid_cnt[ii] += 1
         else:
           self.valid_cnt[ii] = max(self.valid_cnt[ii] - 1, 0)
 
         score = self.rcp.vl[ii+16]['SCORE']
-        # print ii, self.valid_cnt[ii], score, cpt['VALID'], cpt['LONG_DIST'], cpt['LAT_DIST']
 
         # radar point only valid if it's a valid measurement and score is above 50
+        # 【保留原邏輯】: 距離檢查維持 255
         if cpt['VALID'] or (score > 50 and cpt['LONG_DIST'] < 255 and self.valid_cnt[ii] > 0):
           if ii not in self.pts or cpt['NEW_TRACK']:
             self.pts[ii] = RadarData.RadarPoint()
             self.pts[ii].trackId = self.track_id
             self.track_id += 1
-          self.pts[ii].dRel = cpt['LONG_DIST']  # from front of car
-          self.pts[ii].yRel = -cpt['LAT_DIST']  # in car frame's y axis, left is positive
+          
+          # 這裡的值已經是經過新 DBC 高精度轉換後的結果
+          self.pts[ii].dRel = cpt['LONG_DIST']
+          self.pts[ii].yRel = -cpt['LAT_DIST']
           self.pts[ii].vRel = cpt['REL_SPEED']
           self.pts[ii].aRel = float('nan')
           self.pts[ii].yvRel = float('nan')
