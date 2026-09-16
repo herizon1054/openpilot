bbfc33a6dd170ac823fa48dfd06b130cc48915ad
herizon1054 <spivbaby@gmail.com>
2026-09-17 02:29:00 +0800

    Update desire_helper.py

--- selfdrive/controls/lib/desire_helper.py
+++ selfdrive/controls/lib/desire_helper.py
@@ -67,7 +67,12 @@ class DesireHelper:
         blindspot_right=(carstate.rightBlindspot or right_edge_detected),
         left_blinker=carstate.leftBlinker,
         right_blinker=carstate.rightBlinker,
-        v_ego=v_ego
+        v_ego=v_ego,
+        # dp fork divergence：LTD 現在也需要跟 LCA 一樣的方向盤出力確認，
+        # 再加上實際轉角門檻，見 lane_turn_desire.py 的 LaneTurnState 狀態機。
+        steering_pressed=carstate.steeringPressed,
+        steering_torque=carstate.steeringTorque,
+        steering_angle_deg=carstate.steeringAngleDeg,
     )
     lane_turn_desire = self.lane_turn_controller.get_turn_desire()
 
